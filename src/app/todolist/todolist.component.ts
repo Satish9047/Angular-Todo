3c7bf7bac40a87542bb807cde0e46058f73e7c19
Satish9047 <satishprajapati930@gmail.com>
2024-06-07 15:43:43 +0545

    add todo functionality + show taskLists + count total remaining task

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from "@angular/common";
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, NgForm } from '@angular/forms';
 
+
+type ITask = {
+  taskName: string,
+  isCompleted: boolean
+}
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [],
-  templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.css'
+  imports: [FormsModule, CommonModule],
+  templateUrl: './todolist.component.html'
 })
 export class TodolistComponent {
+  task: string = ""
+  taskList: ITask[] = [];
+  totalTasks: number = 0;
+
+
+  addTask(form: NgForm) {
+    if (form.controls['task'].value.trim() === "") {
+      return;
+    }
+    this.taskList.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+    });
+    form.resetForm();
+    this.updateRemaningtasks();
+  }
+
+  updateRemaningtasks() {
+    this.totalTasks = this.taskList.length;
+  }
 
 }
