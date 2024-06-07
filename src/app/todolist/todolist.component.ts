72b0cd7a6e4d0c4c6d02d3e4a9ef1f614f812155
Satish9047 <satishprajapati930@gmail.com>
2024-06-07 19:24:33 +0545

    add completed task + total completed task + delete completed task

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -14,11 +14,14 @@ type ITask = {
   templateUrl: "./todolist.component.html"
 })
 export class TodolistComponent {
+  //INITIAL STATE
   task: string = ""
   taskList: ITask[] = [];
+  completedTaskList: ITask[] = [];
   totalTasks: number = 0;
+  totalCompletedTasks: number = 0;
 
-
+  // ADD TASK
   addTask(form: NgForm) {
     if (form.controls["task"].value.trim() === "") {
       return;
@@ -28,26 +31,56 @@ export class TodolistComponent {
       isCompleted: false,
     });
     this.task = "";
-    this.updateRemaningtasks();
+    this.updateTotalRemaningTasks();
   }
 
+  // DELETE TASK
   deleteTask(index: number) {
     console.log(index);
     this.taskList.splice(index, 1);
-    this.updateRemaningtasks();
+    this.updateTotalRemaningTasks();
   }
 
+  // CHECK TO COMPLETE TASK
   onCheck(index: number) {
     this.taskList[index].isCompleted = !this.taskList[index].isCompleted;
+    this.updateCompletedTasks(index);
     console.log(this.taskList);
+    this.updateTotalRemaningTasks()
   }
 
-  private updateRemaningtasks() {
+  // DELETE COMPLETED TASK
+  deleteCompletedTask(index: number) {
+    this.completedTaskList.splice(index, 1);
+    this.updateTotalRemaningTasks();
+    this.updateTotalCompletedTasks()
+
+  }
+
+  // UNCHECK COMPLETED TASK TO TASK
+  unCheckCompletedTask(index: number) {
+    this.taskList.push(this.completedTaskList[index]);
+    this.completedTaskList.splice(index, 1);
+    this.updateTotalRemaningTasks();
+    this.updateTotalCompletedTasks()
+  }
+
+  // UPDATE TO COMPLETED TASKS
+  private updateCompletedTasks(index: number) {
+    this.completedTaskList.push(this.taskList[index]);
+    this.taskList.splice(index, 1);
+    this.updateTotalCompletedTasks()
+    // console.log("completed  task", this.completedTaskList);
+  }
+
+
+  // TOTAL REMANING TASKS
+  private updateTotalRemaningTasks() {
     this.totalTasks = this.taskList.length;
   }
 
-  // private updateCompletedTasks() {
-
-  // }
-
+  // TOTAL COMPLETED TASKS
+  private updateTotalCompletedTasks() {
+    this.totalCompletedTasks = this.completedTaskList.length;
+  }
 }
